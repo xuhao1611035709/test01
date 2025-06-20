552ee45113900bf05ee602196c717bacc9384e9d
xuhao <hao@exp.ac.cn>
2025-06-20 13:43:50 +0800

    测试 创建main分支，用于测试cicd是否生效

--- src/main/java/com/jwvdp/books/controller/UserController.java
+++ src/main/java/com/jwvdp/books/controller/UserController.java
@@ -1,7 +1,5 @@
-/*
 package com.jwvdp.books.controller;
 
-import com.jwvdp.books.utils.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    UserService userService;
+
 
     @PostMapping("/register")
-    public ResultUtil register(String username, String password) {
-        userService.register()
-        return ResultUtil.success();
+    public String register(String username, String password) {
+        return "访问成功";
     }
 }
-*/
