f964fc11661c3d202b4c9e087d209dd64625aacf
xuhao <hao@exp.ac.cn>
2025-06-20 11:02:35 +0800

    测试 创建main分支，用于测试cicd是否生效

--- src/main/java/com/jwvdp/books/BooksApplication.java
+++ src/main/java/com/jwvdp/books/BooksApplication.java
@@ -2,7 +2,7 @@ package com.jwvdp.books;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@MapperScan("com.jwvdp.xinma.dao")
+
 @SpringBootApplication
 public class BooksApplication {
 
