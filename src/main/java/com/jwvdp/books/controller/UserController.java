78fc80d524d7a5afbc757917d92fce85954bad2a
xuhao <hao@exp.ac.cn>
2025-06-20 16:56:39 +0800

    测试 测试阿里云oss的region未填写问题

--- src/main/java/com/jwvdp/books/controller/UserController.java
+++ src/main/java/com/jwvdp/books/controller/UserController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
-
-
+    
     @PostMapping("/register")
     public String register(String username, String password) {
         return "访问成功";
